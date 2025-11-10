5d5d39c92db307a7916c0c7982bab87c504eada4
Arki <mcfeliciano3@up.edu.ph>
2025-11-10 16:32:40 +0800

    printed this is arki in main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,22 @@
 import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
+
+import java.awt.*;
 
 public class Main extends Application {
     @Override
     public void start(Stage primaryStage) {
         primaryStage.setTitle("My JavaFX App");
-        
+
+        Label message = new Label("Hello, this is Arki!");
+
+        StackPane root = new StackPane(message);
+        Scene scene = new Scene(root, 400, 400);
+
+        primaryStage.setScene(scene);
         primaryStage.show();
     }
 
